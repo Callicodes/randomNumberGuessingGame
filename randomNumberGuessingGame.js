9e37170903e16dc8425e194cc3557ac9120f55be
Aurelius Calliste <callicodes@gmail.com>
2017-04-05 12:15:50 +0100

    Added a clue for the user
    else if ( parseInt(guess) < randomNumber ) {
    var guessMore = prompt('Try again. The number I am thinking of is more
    than ' + guess);
    if (parseInt (guessMore) === randomNumber) {correctGuess = true;}}

--- randomNumberGuessingGame.js
+++ randomNumberGuessingGame.js
@@ -1,12 +1,19 @@
-var randomnumber = Math.floor(Math.random() * 6 ) + 1;
+var correctGuess = false;
+var randomNumber = Math.floor(Math.random() * 6 ) + 1;
 var guess = prompt('I am thinking of a number between 1 and 6. What is it?');
-
-if (parseInt(guess) === randomnumber )
-  
- {document.write('<p>You guessed the number!</p>');
-
-}
-else {
-  document.write('<p>Sorry. The number was ' + randomnumber + '</p>');
-
+if (parseInt(guess) === randomNumber) {
+  correctGuess = true;
+} 
+else if ( parseInt(guess) < randomNumber ) {
+ var guessMore = prompt('Try again. The number I am thinking of is more than ' + guess);
+if (parseInt (guessMore) === randomNumber) {correctGuess = true;}}
+else if (parseInt(guess) > randomNumber) {
+var guessLess = prompt('Try again. The number I am thinking of is less than ' + guess);
+if (parseInt(guessLess) === randomNumber ) {
+    correctGuess = true;}
+    }
+if (correctGuess) {
+    document.write('<p>You guessed the number!</p>');
+} else {
+    document.write('<p>Sorry. The number was ' + randomNumber + '.</p>');
 }
