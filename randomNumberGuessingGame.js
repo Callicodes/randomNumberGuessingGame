7469b373d05cb3000dcfe9b2fc92188294ae58dd
Aurelius Calliste <callicodes@gmail.com>
2017-04-06 16:53:08 +0100

    Notes
    practising passing arguments to functions

--- randomNumberGuessingGame.js
+++ randomNumberGuessingGame.js
@@ -2,5 +2,17 @@ function getRandomNumber( upper ) {
   var randomNumber = Math.floor( Math.random() * upper ) + 1; 
   return randomNumber;
 }
-
+/*
 console.log( getRandomNumber(6) );
+*/
+
+/*multiply the length of a triangle by it's area*/
+
+function getArea(width, length) {
+	var area = width * length;
+	return area;
+}
+
+console.log(getArea(10, 20));
+
+
